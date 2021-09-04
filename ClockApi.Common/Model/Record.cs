3d88c083067b3f2e7192a5b9702e4064733bd7cb
smurillo29 <santimv2907@gmail.com>
2021-09-04 18:56:18 -0500

    add record model and response

---
+++ ClockApi.Common/Model/Record.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClockApi.Common.Model
+{
+    internal class Record
+    {
+        public int Id { get; set; }
+
+        public DateTime DateTimeRecord { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
