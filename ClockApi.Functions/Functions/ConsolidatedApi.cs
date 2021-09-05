94d5293fd81208953c71fb7774973974a1408815
smurillo29 <santimv2907@gmail.com>
2021-09-05 14:55:14 -0500

    Add consolidated process

--- ClockApi.Functions/Functions/ConsolidatedApi.cs
+++ ClockApi.Functions/Functions/ConsolidatedApi.cs
@@ -1,12 +1,16 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using ClockApi.Common.Responses;
+using ClockApi.Functions.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace ClockApi.Functions.Functions
 {
@@ -14,22 +18,86 @@ namespace ClockApi.Functions.Functions
     {
         [FunctionName("ConsolidatedApi")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "consolidated")] HttpRequest req,
+            [Table("record", Connection = "AzureWebJobsStorage")] CloudTable recordTable,
+            [Table("consolidated", Connection = "AzureWebJobsStorage")] CloudTable consolidatedTable,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation($"Consolidated completed function executed at: {DateTime.Now}");
+            string filter = TableQuery.GenerateFilterConditionForBool("IsConsolidated", QueryComparisons.Equal, false);
+            TableQuery<RecordEntity> query = new TableQuery<RecordEntity>().Where(filter);
+            TableQuerySegment<RecordEntity> records = await recordTable.ExecuteQuerySegmentedAsync(query, null);
+            List<RecordEntity> orderListRecords = records.Results.OrderBy(x => x.Id).ThenBy(x => x.DateTimeRecord).ToList();
 
-            string name = req.Query["name"];
+            List<ConsolidatedEntity> consolidate = null;
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            RecordEntity lastRecord = null;
+            foreach (RecordEntity record in orderListRecords)
+            {
+                if (lastRecord == null)
+                {
+                    lastRecord = record;
+                }
+                else if ((lastRecord.Id == record.Id) && (lastRecord.Type != record.Type))
+                {
+                    int hoursWorked = TimeBetweenDates(lastRecord.DateTimeRecord, record.DateTimeRecord);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                    string filter2 = TableQuery.GenerateFilterConditionForInt("ID", QueryComparisons.Equal, record.Id);
+                    TableQuery<ConsolidatedEntity> queryConsilidates = new TableQuery<ConsolidatedEntity>().Where(filter2);
+                    TableQuerySegment<ConsolidatedEntity> querys = await consolidatedTable.ExecuteQuerySegmentedAsync(queryConsilidates, null);
+                    consolidate = querys.Results.Where(x => x.Date.Date == DateTime.UtcNow.Date).ToList();
 
-            return new OkObjectResult(responseMessage);
+                    if (consolidate.Count() == 0)
+                    {
+                        ConsolidatedEntity consolidateEntity = new ConsolidatedEntity
+                        {
+                            Date = System.DateTime.UtcNow,
+                            ID = record.Id,
+                            Minutes = hoursWorked,
+                            ETag = "*",
+                            PartitionKey = "CON",
+                            RowKey = System.Guid.NewGuid().ToString(),
+
+                        };
+                        TableOperation addOperation = TableOperation.Insert(consolidateEntity);
+                         await consolidatedTable.ExecuteAsync(addOperation);
+
+                    }
+                    else
+                    {
+                        ConsolidatedEntity con = consolidate[0];                        
+                        con.Minutes += hoursWorked;                        
+                        TableOperation updateOperation = TableOperation.Replace(con);
+                        await consolidatedTable.ExecuteAsync(updateOperation);
+                    }
+
+
+                    lastRecord = null;
+                }
+                else
+                {
+                    lastRecord = null;
+                }
+
+            }
+
+
+
+
+
+            return new OkObjectResult(new Response
+            {
+                IsSuccess = true,
+                Message = "ejemplo",
+                Result = orderListRecords
+
+            });
+        }
+
+        public static int TimeBetweenDates(DateTime initialDate, DateTime finaldate)
+        {
+            TimeSpan diference = finaldate - initialDate;
+            return (int)diference.TotalMinutes;
         }
     }
 }
