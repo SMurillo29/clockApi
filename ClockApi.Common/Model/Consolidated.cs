8ebd24d56aa8c13fbebe74fc43ee2e9e2d4af627
smurillo29 <santimv2907@gmail.com>
2021-09-04 22:50:42 -0500

    Add Consolidated Model, Entity, and Function

---
+++ ClockApi.Common/Model/Consolidated.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClockApi.Common.Model
+{
+    internal class Consolidated
+    {
+        // ID Empleo(int)       
+        // Fecha(date time)
+        // Minutos trabajados(int)
+
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public int Minutes { get; set; }
+
+    }
+}
---
+++ ClockApi.Functions/Entities/ConsolidatedEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace ClockApi.Functions.Entities
+{
+    public class ConsolidatedEntity : TableEntity
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public int Minutes { get; set; }
+
+    }
+}
